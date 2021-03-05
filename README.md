43171652d2f94ef60a76751680904056aebbbaa5
yut4916 <katherine.yut@ou.edu>
2021-03-05 04:36:34 +0000

    First save for project0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cs5293sp21-project0
-Project 0 for Text Analytics
+## Project 0 for Text Analytics
+
+
+* sudo apt install pipenv -- used this command to give me permission to install pipenv
+	+ note: sudo su changes me to the super user, all powerful omniscient being. be careful (type "exit" in command line to becom emortal)
+* tmux kill-session -t 0 -- kill a tmux session (0 is the window ID)
+
---
+++ project0/main_messy.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# Example main.py
+import argparse
+
+import project0
+
+import urllib
+
+import tempfile
+
+import PyPDF2
+
+def fetchincidents(url):
+
+    url = ("https://www.normanok.gov/sites/default/files/documents/"
+           "2021-02/2021-02-21_daily_incident_summary.pdf")
+
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+
+    data = urllib.request.urlopen(urllib.request.Request(url, headers=headers)).read()
+
+
+def extractincidents(incident_data):
+    fp = tempfile.TemporaryFile()
+
+    # Write the pdf data to a temp file
+    fp.write(data.read())
+
+    # Set the curser of the file back to the begining
+    fp.seek(0)
+
+    # Read the PDF
+    pdfReader = PyPDF2.pdf.PdfFileReader(fp)
+    pdfReader.getNumPages()
+
+    # Get the first page
+    page1 = pdfReader.getPage(0).extractText()
+    # ...
+
+def createdb():
+ #    CREATE TABLE incidents (
+        incident_time TEXT,
+        incident_number TEXT,
+        incident_location TEXT,
+        nature TEXT,
+        incident_ori TEXT
+    )
+
+
+def main(url):
+    # Download data
+    incident_data = project0.fetchincidents(url)
+
+    # Extract data
+    incidents = project0.extractincidents(incident_data)
+	
+    # Create new database
+    db = project0.createdb()
+	
+    # Insert data
+    project0.populatedb(db, incidents)
+	
+    # Print incident counts
+    project0.status(db)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--incidents", type=str, required=True, 
+                         help="Incident summary url.")
+     
+    args = parser.parse_args()
+    if args.incidents:
+        main(args.incidents)
+
+
+
+
